f79018143c0d4da48ee4b087c26517d43678428e
Brendan Bergen <icantthinkofaname@gmail.com>
2022-12-04 10:47:25 -0500

    Update day1_part1.py

--- src/day1/day1_part1.py
+++ src/day1/day1_part1.py
@@ -3,11 +3,8 @@ currentElf = 0
 
 with open('input.txt','r') as f:
     for line in f:
-      #  print(line,end='')
         if line == '\n':
-            print('new elf')
             if currentElf > maxElf:
-      #          print('new max elf! ', currentElf)
                 maxElf = currentElf
             currentElf = 0
         else:
